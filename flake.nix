a778c473f3a42ab1514615db552807553c9df500
Christian Taylor <christianjtaylor@sky.com>
2024-06-01 08:46:00 +0100

    start building flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Dotnet devshells";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+	  pkgs = import nixpkgs {
+	    inherit system;
+	  };
+	in
+	with pkgs;
+	{
+	  devShells.default = mkShell {
+	    buildInputs = [
+	      dotnet-sdk_6
+	      omnisharp-roslyn
+	      mono
+	      msbuild
+	      act
+	    ];
+
+	    DOTNET_ROOT="${dotnet-sdk_6}";
+
+	    shellHook = ''
+	      dotnet pack -c Release ./src -o ~/workarea/nuget-packages/
+	    '';
+	  };
+	}
+      );
+}
